b26cb8845f3841ec90eef045ee7ef6a1199eb1ef
7FOX7 <kheeugene@gmail.com>
2024-05-25 18:19:55 -0400

    Added code to enable/disable button if the form is filled

--- scripts/createAccount.js
+++ scripts/createAccount.js
@@ -48,7 +48,12 @@ $('.js-stretchbox').click(function() {
 
 $(document).ready(function() {
     $('.js-remove-image-button').css("display", "none");
+    $('#createAccountContinueButton')
+        .prop('disabled', true)
+        .css({'background-image': 'none', 'background-color': 'rgba(0, 0, 0, 0.2)'}); 
 
+    
+    
     inputFile.addEventListener('change', (e) => {
         const imageUrl = URL.createObjectURL(e.target.files[0]); 
         if(!imageUrl) {
@@ -95,9 +100,7 @@ $(document).ready(function() {
 
     function handleInvalidName(nameField) {
         const errorMessage = $(nameField).closest('.js-input-section').find('.js-error-message'); 
-        $(nameField).on('keyup', function() {
-             removeRedBorder(nameField); 
-        })
+        onFocus(nameField);
         $(nameField).on('focusout', function() {
             var stringValue = $(nameField).val(); 
             if(stringValue === "") {
@@ -112,24 +115,40 @@ $(document).ready(function() {
                 errorMessage.removeClass('active'); 
                 removeRedBorder(nameField); 
             }
+            if(allFilled()) {
+                $('#createAccountContinueButton')
+                    .removeAttr('disabled')
+                    .css({"background-image": "linear-gradient(to bottom right, red, hotpink)"}); 
+            }
+            else {
+                $('#createAccountContinueButton').prop('disabled', 'disabled'); 
+            }
         })
     }
 
     function handleInvalidEmail(emailField) {
         const errorMessage = $(emailField).closest('.js-input-section').find('.js-error-message'); 
         const regex =  /^\S+@\S+\.\S+$/; 
-        $(emailField).on('keyup', function() {
-            removeRedBorder(emailField);
-        });
+        onFocus(emailField);
         $(emailField).on('focusout', function() {
             var stringValue = $(emailField).val(); 
             if(!regex.test(stringValue)) {
                 errorMessage.addClass('active'); 
+                $('#createAccountContinueButton').prop('disabled', 'disabled');
                 addRedBorder(emailField); 
             }
             else {
                 errorMessage.removeClass('active'); 
+                $('#createAccountContinueButton').removeAttr('disabled'); 
                 removeRedBorder(emailField); 
+            }
+            if(allFilled() && regex.test(stringValue)) {
+                $('#createAccountContinueButton')
+                    .removeAttr('disabled')
+                    .css({"background-image": "linear-gradient(to bottom right, red, hotpink)"});  
+            }
+            else {
+                $('#createAccountContinueButton').prop('disabled', 'disabled'); 
             }
         })
     }
@@ -140,8 +159,8 @@ $(document).ready(function() {
         // const regex = /\b(10|11|12)\b/g;
         var stringValue;
         const regex = /^(0[1-9]|[1-9]|1[0-2])$/; 
+        onFocus(month);
         $(month).on('keyup', function() { 
-            removeRedBorder(month);
             stringValue = $(month).val(); 
             if(regex.test(stringValue) && stringValue.length === 1) {
                 errorMessage.removeClass('active'); 
@@ -168,6 +187,15 @@ $(document).ready(function() {
                 errorMessage.addClass('active'); 
                 addRedBorder(month);
             }
+
+            if(allFilled()) {
+                $('#createAccountContinueButton')
+                    .removeAttr('disabled')
+                    .css({"background-image": "linear-gradient(to bottom right, red, hotpink)"});  
+            }
+            else {
+                $('#createAccountContinueButton').prop('disabled', 'disabled'); 
+            }
         })   
     }
 
@@ -176,8 +204,8 @@ $(document).ready(function() {
         const nextField = yearOfBirth; 
         const regex = /^(0[1-9]|[1-9]|1[0-9]|2[0-9]|3[01])$/; 
         var stringValue; 
+        onFocus(day);
         $(day).on('keyup', function(){
-            removeRedBorder(day);
             stringValue = $(day).val();
             if(regex.test(stringValue) && stringValue.length === 1) {
                 errorMessage.removeClass('active'); 
@@ -204,6 +232,12 @@ $(document).ready(function() {
                 errorMessage.addClass('active'); 
                 addRedBorder(day); 
             }
+
+            allFilled() ? $('#createAccountContinueButton')
+                .removeAttr('disabled')
+                .css({"background-image": "linear-gradient(to bottom right, red, hotpink)"}) 
+                : 
+                $('#createAccountContinueButton').prop('disabled', 'disabled');
         }); 
     }
 
@@ -212,9 +246,9 @@ $(document).ready(function() {
         var stringValue; 
         const regex = /^(1[0-9][0-9][0-9]|2[0][0-2][0-9])$/; 
 
+        onFocus(year); 
         $(year).on('keyup', function() {
             stringValue = $(year).val(); 
-            removeRedBorder(year); 
             if(!regex.test(stringValue)) {
                 errorMessage.addClass('active');  
             }
@@ -231,7 +265,23 @@ $(document).ready(function() {
                 errorMessage.addClass('active'); 
                 addRedBorder(year);
             }
+
+            allFilled() ?  $('#createAccountContinueButton')
+                .removeAttr('disabled')
+                .css({"background-image": "linear-gradient(to bottom right, red, hotpink)"}) 
+                :
+                 $('#createAccountContinueButton').prop('disabled', 'disabled')
         })
+    }
+
+    function allFilled() {
+        var filled = true; 
+        $('.js-required-field').each(function() {
+            if($(this).val() === '') {
+                filled = false; 
+            }
+        });
+        return filled; 
     }
 
     function removeRedBorder(field) {
@@ -241,8 +291,13 @@ $(document).ready(function() {
     function addRedBorder(field) {
         $(field).css("border-color", "red"); 
     }
-}); 
 
+    function onFocus(field) {
+        $(field).on('focus', function() {
+            removeRedBorder(field);
+        }); 
+    }
+}); 
 
 
 /*
@@ -293,9 +348,6 @@ jQuery.fn.extend({
     })
 
   
-
-
-
     <label> refers to the <div> element. div element contains an id
 
     so the div with the id equals to 15, label id will be equal to 15 too

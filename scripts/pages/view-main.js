588e9e4c558b04c340a08cdd2b7fe1acb17955ed
7FOX7 <kheeugene@gmail.com>
2024-06-02 12:56:27 -0400

    Started working on the keen slider

---
+++ scripts/pages/view-main.js
@@ -0,0 +1,141 @@
+///////////////////////////////////////////////////////////////////////////////////////
+// make the buttons increase/decrease the size on hover:
+const interactiveBtns = document.querySelectorAll('.js-interactive-button'); 
+
+interactiveBtns.forEach((button) => {
+    const interactionIcon = button.querySelector('.js-interaction-icon');
+    interactionIcon.style.transition = "0.3s"; 
+    button.addEventListener('mouseenter', () => { 
+        interactionIcon.style.transform = "scale(1.2)"; 
+    });
+    button.addEventListener('mouseleave', () => {
+        interactionIcon.style.transform = "none"; 
+    }); 
+})
+
+/////////////////////////////////////////////////////////////////////////////////////
+// make the slider buttons appear/disappear on hover:
+const keenSlider = document.querySelector('.js-keen-slider'); 
+const keenSliderContainer = document.querySelector('.js-keen-slider-button-container'); 
+
+keenSlider.addEventListener('mouseenter', () => {
+    const animationPromise = keenSliderContainer.animate(makeSmoothButtonAppearance, keenSliderContainer_timing).finished; 
+    animationPromise.then(() => {
+        keenSliderContainer.classList.add('active');
+    })
+}); 
+
+keenSlider.addEventListener('mouseleave', () => {
+    const animationPromise = keenSliderContainer.animate(makeSmoothButtonDisappearance, keenSliderContainer_timing).finished; 
+    animationPromise.then(() => {
+        keenSliderContainer.classList.remove('active');
+    })
+});
+
+const makeSmoothButtonAppearance = [
+    {opacity: "0"},
+    {opacity: "0.5"},
+    {opacity: "1"}
+];
+const makeSmoothButtonDisappearance = [
+    {opacity: "1"},
+    {opacity: "0.5"},
+    {opacity: "0"} 
+];
+
+const keenSliderContainer_timing = {
+    duration: 200
+};
+
+/////////////////////////////////////////////////////////////////////////////
+// make the slider buttons interactive: 
+document.addEventListener('DOMContentLoaded', () => {
+    let sliderPosition = 0; 
+    const sliderImageArr = [];
+    const images = document.querySelectorAll('.js-keen-slider-item'); 
+
+    images.forEach((image) => {
+        sliderImageArr.push(image)
+    }); 
+
+    console.log(sliderImageArr.length); 
+
+    const nextSlide_Button = document.querySelector('.js-next-slider-button');
+
+    nextSlide_Button.addEventListener('click', () => {
+        if(sliderPosition === sliderImageArr.length) {
+            nextSlide_Button.style.visibility = "hidden"; 
+        }
+        else {
+            nextSlide_Button.style.visibility = "visible";
+            sliderPosition++; 
+            updateSlide(); 
+        }
+    })
+
+    const previousSlide_Button = document.querySelector('.js-previous-slider-button'); 
+    previousSlide_Button.addEventListener('click', () => {
+        if(sliderPosition === 0) {
+            previousSlide_Button.style.visibility = "hidden"; 
+        }
+        else {
+            previousSlide_Button.style.visibility = "visible"; 
+            sliderPosition--; 
+            updateSlide(); 
+        }
+    }); 
+
+    function updateSlide() {
+        sliderImageArr.forEach((image) => {
+            const index = sliderImageArr.indexOf(image); 
+            if(index !== sliderPosition) {
+                image[index].style.display = "none"; 
+            }
+            else {
+                image[index].style.display = "block"; 
+            }
+        })
+    }
+    /*
+    let slidePosition = 0; 
+    const imgArr = []; 
+    const images = document.querySelectorAll(images)
+    images.forEach((image) => {
+        imgArr.push(image)
+    });
+
+    const nextButton = document.querySelector('.js-next-button')
+        .addEventListener('click', () => {
+            if(sliderPosition === imgArr.length) {
+                nextButton.style.display = "none"; 
+            }
+            else {
+                sliderPosition++; 
+                updateSlide(); 
+            } 
+        }); 
+
+    function updateSlide() {
+        imgArr.forEach((image) => {
+            const index = imgArr.indexOf(image); 
+            if(index !== slidePosition) {
+                image[index].style.display = "none"; 
+            }
+            else {
+                image[index].style.display = "flex"; 
+            }
+        })
+    }
+
+    const previousButton = document.querySelector('.js-previous-button')
+        .addEventListener('click', () => {
+            if(sliderPosition === 0) {
+                previousButton.style.display = "none"; 
+            }
+            else {
+                sliderPosition --; 
+                updateSlide();  
+            }
+        })
+    */
+});

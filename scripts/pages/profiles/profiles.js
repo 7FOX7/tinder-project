04d00636fa967206598f5c87f6becfec77e26a1e
Evgenii Khe <ekhe1@ncstudents.niagaracollege.ca>
2024-06-07 21:46:12 -0400

    added file

---
+++ scripts/pages/profiles/profiles.js
@@ -0,0 +1,299 @@
+export const profiles = [
+    {profile: `
+        <div class="js-profile-container profile-container">
+            <div class="stamp js-stamp" style="top: -12%; left: 45%; transform: rotate(20deg);">
+                <div style="background-image: url(../images/nope_icon.png); background-size: cover; width: 100%; height: 100%;"></div>
+            </div>
+            <div class="stamp js-stamp" style="top: -12%; left: -20%; transform: rotate(-20deg);">
+                <div style="background-image: url(../images/like_icon.png); background-size: cover; width: 100%; height: 100%;"></div>
+            </div>
+            <div class="stamp js-stamp" style="top: 30%; left: 15%; transform: rotate(-20deg);">
+                <div style="background-image: url(../images/super-like_icon.png); background-size: cover; width: 100%; height: 100%;"></div>
+            </div>
+            <div class="js-slide-dot-container slide-dot-container"></div>
+            <div class="js-keen-slider keen-slider">
+                <span style="display: block" class="keen-slider-item keen-slider-item--hidden js-keen-slider-item">
+                    <div class="image sampleImg1"></div>
+                </span>
+                <span class="keen-slider-item keen-slider-item--hidden js-keen-slider-item">
+                    <div class="image sampleImg2"></div>
+                </span>
+                <span class="keen-slider-item keen-slider-item--hidden js-keen-slider-item">
+                    <div class="image sampleImg3"></div>
+                </span>
+                <div class="keen-slider-button-container js-keen-slider-button-container">
+                    <button class="js-previous-slider-button keen-slider-button">
+                        <span>
+                            <img src="../images/arrow_previous.png">
+                        </span>
+                    </button>
+                    <button class="js-next-slider-button keen-slider-button" >
+                        <span>
+                            <img src="../images/arrow_next.png">
+                        </span>
+                    </button>
+                </div>
+            </div>
+            <div style="z-index: 5; background-image: linear-gradient(to top, rgb(0, 0, 0) 0%, rgba(255, 255, 255, 0) 100%); width: 100%; position: absolute; height: 25%; left: 0; top: 61%;"></div>
+            <div class="js-user-info-container user-info-container">
+                <div class="user-info-content">
+                    <div style="display: flex; align-items: center;">
+                        <div style="width: 10px; height: 10px; background-color: rgb(48, 255, 48); margin-right: 5px; border-radius: 50%;"></div>
+                        <span>Recently Active</span>
+                    </div>
+                    <div style="font-size: 2em;">
+                        <span style="font-weight: bold">Evgenii</span>
+                        <span>19</span>
+                        <span style="margin-left: 2px;">
+                            <img style="width: 26px;" src="../images/verified_icon.png">
+                        </span>
+                        <div class="js-interactive-button" role="button" style="float: right;">
+                            <button style="display: flex; align-items: center; padding: 0; background-color: transparent;">
+                                <span class="js-interaction-icon">
+                                    <img src="../images/more_info_icon.png" style="width: 30px; height: 30px;">
+                                </span>
+                            </button>
+                        </div>
+                    </div>
+                    <div style="display: flex; align-items: center; font-size: 15px;">
+                        <span style="margin-right: 5px;">
+                            <img style="width: 17px;" src="../images/home_icon.png">
+                        </span>
+                        <span>Lives in Toronto</span>
+                    </div>
+                    <div style="display: flex; align-items: center; font-size: 15px;">
+                        <span style="margin-right: 5px;">
+                            <img style="width: 17px;" src="../images/location_icon.png">
+                        </span>
+                        <span>4 kilometers away</span>
+                    </div>
+                </div>
+            </div>
+            <div style="position: absolute; top: 85%; width: 100%; background-color: #000; height: 15%; border-bottom-left-radius: 15px; border-bottom-right-radius: 15px;"></div>
+        </div>
+    `}, 
+    {profile: `
+        <div class="js-profile-container profile-container">
+            <div class="stamp js-stamp" style="top: -12%; left: 45%; transform: rotate(20deg);">
+                <div style="background-image: url(../images/nope_icon.png); background-size: cover; width: 100%; height: 100%;"></div>
+            </div>
+            <div class="stamp js-stamp" style="top: -12%; left: -20%; transform: rotate(-20deg);">
+                <div style="background-image: url(../images/like_icon.png); background-size: cover; width: 100%; height: 100%;"></div>
+            </div>
+            <div class="stamp js-stamp" style="top: 30%; left: 15%; transform: rotate(-20deg);">
+                <div style="background-image: url(../images/super-like_icon.png); background-size: cover; width: 100%; height: 100%;"></div>
+            </div>
+            <div class="js-slide-dot-container slide-dot-container"></div>
+            <div class="js-keen-slider keen-slider">
+                <span style="display: block" class="keen-slider-item keen-slider-item--hidden js-keen-slider-item">
+                    <div class="image sampleImg1"></div>
+                </span>
+                <span class="keen-slider-item keen-slider-item--hidden js-keen-slider-item">
+                    <div class="image sampleImg2"></div>
+                </span>
+                <span class="keen-slider-item keen-slider-item--hidden js-keen-slider-item">
+                    <div class="image sampleImg3"></div>
+                </span>
+                <div class="keen-slider-button-container js-keen-slider-button-container">
+                    <button class="js-previous-slider-button keen-slider-button">
+                        <span>
+                            <img src="../images/arrow_previous.png">
+                        </span>
+                    </button>
+                    <button class="js-next-slider-button keen-slider-button" >
+                        <span>
+                            <img src="../images/arrow_next.png">
+                        </span>
+                    </button>
+                </div>
+            </div>
+            <div style="z-index: 5; background-image: linear-gradient(to top, rgb(0, 0, 0) 0%, rgba(255, 255, 255, 0) 100%); width: 100%; position: absolute; height: 25%; left: 0; top: 61%;"></div>
+            <div class="js-user-info-container user-info-container">
+                <div class="user-info-content">
+                    <div style="display: flex; align-items: center;">
+                        <div style="width: 10px; height: 10px; background-color: rgb(48, 255, 48); margin-right: 5px; border-radius: 50%;"></div>
+                        <span>Recently Active</span>
+                    </div>
+                    <div style="font-size: 2em;">
+                        <span style="font-weight: bold">Evgenii</span>
+                        <span>19</span>
+                        <span style="margin-left: 2px;">
+                            <img style="width: 26px;" src="../images/verified_icon.png">
+                        </span>
+                        <div class="js-interactive-button" role="button" style="float: right;">
+                            <button style="display: flex; align-items: center; padding: 0; background-color: transparent;">
+                                <span class="js-interaction-icon">
+                                    <img src="../images/more_info_icon.png" style="width: 30px; height: 30px;">
+                                </span>
+                            </button>
+                        </div>
+                    </div>
+                    <div style="display: flex; align-items: center; font-size: 15px;">
+                        <span style="margin-right: 5px;">
+                            <img style="width: 17px;" src="../images/home_icon.png">
+                        </span>
+                        <span>Lives in Toronto</span>
+                    </div>
+                    <div style="display: flex; align-items: center; font-size: 15px;">
+                        <span style="margin-right: 5px;">
+                            <img style="width: 17px;" src="../images/location_icon.png">
+                        </span>
+                        <span>4 kilometers away</span>
+                    </div>
+                </div>
+            </div>
+            <div style="position: absolute; top: 85%; width: 100%; background-color: #000; height: 15%; border-bottom-left-radius: 15px; border-bottom-right-radius: 15px;"></div>
+        </div>
+    `}, 
+    {profile: `
+       <div class="js-profile-container profile-container">
+            <div class="stamp js-stamp" style="top: -12%; left: 45%; transform: rotate(20deg);">
+                <div style="background-image: url(../images/nope_icon.png); background-size: cover; width: 100%; height: 100%;"></div>
+            </div>
+            <div class="stamp js-stamp" style="top: -12%; left: -20%; transform: rotate(-20deg);">
+                <div style="background-image: url(../images/like_icon.png); background-size: cover; width: 100%; height: 100%;"></div>
+            </div>
+            <div class="stamp js-stamp" style="top: 30%; left: 15%; transform: rotate(-20deg);">
+                <div style="background-image: url(../images/super-like_icon.png); background-size: cover; width: 100%; height: 100%;"></div>
+            </div>
+            <div class="js-slide-dot-container slide-dot-container"></div>
+            <div class="js-keen-slider keen-slider">
+                <span style="display: block" class="keen-slider-item keen-slider-item--hidden js-keen-slider-item">
+                    <div class="image sampleImg1"></div>
+                </span>
+                <span class="keen-slider-item keen-slider-item--hidden js-keen-slider-item">
+                    <div class="image sampleImg2"></div>
+                </span>
+                <span class="keen-slider-item keen-slider-item--hidden js-keen-slider-item">
+                    <div class="image sampleImg3"></div>
+                </span>
+                <div class="keen-slider-button-container js-keen-slider-button-container">
+                    <button class="js-previous-slider-button keen-slider-button">
+                        <span>
+                            <img src="../images/arrow_previous.png">
+                        </span>
+                    </button>
+                    <button class="js-next-slider-button keen-slider-button" >
+                        <span>
+                            <img src="../images/arrow_next.png">
+                        </span>
+                    </button>
+                </div>
+            </div>
+            <div style="z-index: 5; background-image: linear-gradient(to top, rgb(0, 0, 0) 0%, rgba(255, 255, 255, 0) 100%); width: 100%; position: absolute; height: 25%; left: 0; top: 61%;"></div>
+            <div class="js-user-info-container user-info-container">
+                <div class="user-info-content">
+                    <div style="display: flex; align-items: center;">
+                        <div style="width: 10px; height: 10px; background-color: rgb(48, 255, 48); margin-right: 5px; border-radius: 50%;"></div>
+                        <span>Recently Active</span>
+                    </div>
+                    <div style="font-size: 2em;">
+                        <span style="font-weight: bold">Evgenii</span>
+                        <span>19</span>
+                        <span style="margin-left: 2px;">
+                            <img style="width: 26px;" src="../images/verified_icon.png">
+                        </span>
+                        <div class="js-interactive-button" role="button" style="float: right;">
+                            <button style="display: flex; align-items: center; padding: 0; background-color: transparent;">
+                                <span class="js-interaction-icon">
+                                    <img src="../images/more_info_icon.png" style="width: 30px; height: 30px;">
+                                </span>
+                            </button>
+                        </div>
+                    </div>
+                    <div style="display: flex; align-items: center; font-size: 15px;">
+                        <span style="margin-right: 5px;">
+                            <img style="width: 17px;" src="../images/home_icon.png">
+                        </span>
+                        <span>Lives in Toronto</span>
+                    </div>
+                    <div style="display: flex; align-items: center; font-size: 15px;">
+                        <span style="margin-right: 5px;">
+                            <img style="width: 17px;" src="../images/location_icon.png">
+                        </span>
+                        <span>4 kilometers away</span>
+                    </div>
+                </div>
+            </div>
+            <div style="position: absolute; top: 85%; width: 100%; background-color: #000; height: 15%; border-bottom-left-radius: 15px; border-bottom-right-radius: 15px;"></div>
+        </div>
+    `}, 
+    {profile: `
+        <div class="js-profile-container profile-container">
+            <div class="stamp js-stamp" style="top: -12%; left: 45%; transform: rotate(20deg);">
+                <div style="background-image: url(../images/nope_icon.png); background-size: cover; width: 100%; height: 100%;"></div>
+            </div>
+            <div class="stamp js-stamp" style="top: -12%; left: -20%; transform: rotate(-20deg);">
+                <div style="background-image: url(../images/like_icon.png); background-size: cover; width: 100%; height: 100%;"></div>
+            </div>
+            <div class="stamp js-stamp" style="top: 30%; left: 15%; transform: rotate(-20deg);">
+                <div style="background-image: url(../images/super-like_icon.png); background-size: cover; width: 100%; height: 100%;"></div>
+            </div>
+            <div class="js-slide-dot-container slide-dot-container"></div>
+            <div class="js-keen-slider keen-slider">
+                <span style="display: block" class="keen-slider-item keen-slider-item--hidden js-keen-slider-item">
+                    <div class="image sampleImg1"></div>
+                </span>
+                <span class="keen-slider-item keen-slider-item--hidden js-keen-slider-item">
+                    <div class="image sampleImg2"></div>
+                </span>
+                <span class="keen-slider-item keen-slider-item--hidden js-keen-slider-item">
+                    <div class="image sampleImg3"></div>
+                </span>
+                <div class="keen-slider-button-container js-keen-slider-button-container">
+                    <button class="js-previous-slider-button keen-slider-button">
+                        <span>
+                            <img src="../images/arrow_previous.png">
+                        </span>
+                    </button>
+                    <button class="js-next-slider-button keen-slider-button" >
+                        <span>
+                            <img src="../images/arrow_next.png">
+                        </span>
+                    </button>
+                </div>
+            </div>
+            <div style="z-index: 5; background-image: linear-gradient(to top, rgb(0, 0, 0) 0%, rgba(255, 255, 255, 0) 100%); width: 100%; position: absolute; height: 25%; left: 0; top: 61%;"></div>
+            <div class="js-user-info-container user-info-container">
+                <div class="user-info-content">
+                    <div style="display: flex; align-items: center;">
+                        <div style="width: 10px; height: 10px; background-color: rgb(48, 255, 48); margin-right: 5px; border-radius: 50%;"></div>
+                        <span>Recently Active</span>
+                    </div>
+                    <div style="font-size: 2em;">
+                        <span style="font-weight: bold">Evgenii</span>
+                        <span>19</span>
+                        <span style="margin-left: 2px;">
+                            <img style="width: 26px;" src="../images/verified_icon.png">
+                        </span>
+                        <div class="js-interactive-button" role="button" style="float: right;">
+                            <button style="display: flex; align-items: center; padding: 0; background-color: transparent;">
+                                <span class="js-interaction-icon">
+                                    <img src="../images/more_info_icon.png" style="width: 30px; height: 30px;">
+                                </span>
+                            </button>
+                        </div>
+                    </div>
+                    <div style="display: flex; align-items: center; font-size: 15px;">
+                        <span style="margin-right: 5px;">
+                            <img style="width: 17px;" src="../images/home_icon.png">
+                        </span>
+                        <span>Lives in Toronto</span>
+                    </div>
+                    <div style="display: flex; align-items: center; font-size: 15px;">
+                        <span style="margin-right: 5px;">
+                            <img style="width: 17px;" src="../images/location_icon.png">
+                        </span>
+                        <span>4 kilometers away</span>
+                    </div>
+                </div>
+            </div>
+            <div style="position: absolute; top: 85%; width: 100%; background-color: #000; height: 15%; border-bottom-left-radius: 15px; border-bottom-right-radius: 15px;"></div>
+        </div>
+    `}
+]; 
+
+const keenSliderImageContainer = document.querySelector('.js-keen-slider-container'); 
+profiles.forEach(profile => {
+    keenSliderImageContainer.insertAdjacentHTML('beforeend', profile.profile);  
+})
